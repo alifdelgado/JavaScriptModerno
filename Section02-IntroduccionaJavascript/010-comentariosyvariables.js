9db3169ba21e90ea5ca10f3a19f7e41f7b2987e1
alifdelgado <alifdelgado@gmail.com>
2020-02-20 14:40:22 -0600

    Se inicia la seccion 2 del curso

--- Section02-IntroduccionaJavascript/010-comentariosyvariables.js
+++
@@ -1,3 +0,0 @@
-let a = 10;
-var b = 10;
-const c = 10;
---
+++ assets/js/palabras-reservadas.js
@@ -0,0 +1,10 @@
+console.log('Hola Mundo');
+
+let objeto$ = 123;
+let objeto99_99 = 123;
+
+/** Lower camel case */
+let jugardorConPuntajeMasAlto = 'Fernando';
+
+/** Upper camel case */
+class JuegoAnio{}
