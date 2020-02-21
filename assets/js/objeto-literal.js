e6d3ad45cda1fadca0632f09837c343143c24793
alifdelgado <alifdelgado@gmail.com>
2020-02-21 11:21:53 -0600

    Se finaliza la sección 2 del curso

--- assets/js/objeto-literal.js
+++ assets/js/objeto-literal.js
@@ -1,4 +1,4 @@
-let personaje = {
+const personaje = {
     nombre: 'Tony Stark',
     codeName: 'Ironman',
     vivo: false,
@@ -27,3 +27,26 @@ console.log('Ultimo traje: ', personaje.trajes[personaje.trajes.length - 1]);
 const x = 'vivo';
 console.log('vivo: ', personaje[x]);
 console.log('Ultima pelicula: ', personaje["ultima-pelicula"]);
+
+/** Más detalles */
+delete personaje.edad;
+console.log({personaje});
+
+personaje.casado = true;
+
+const entriesPares = Object.entries(personaje);
+console.log(entriesPares);
+
+console.log(personaje);
+
+Object.freeze(personaje);
+
+personaje.dinero = 10000000000;
+console.log(personaje);
+// delete personaje.direccion.ubicacion;
+personaje.direccion.ubicacion = 'Costa Rica';
+console.log(personaje);
+
+const propiedades = Object.getOwnPropertyNames(personaje);
+const valores = Object.values(personaje);
+console.log({propiedades, valores});
