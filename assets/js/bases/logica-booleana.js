e7d629ff813720e141757dc1826db7d8b29bc30e
alifdelgado <alifdelgado@gmail.com>
2020-03-04 11:19:10 -0600

    Se termina la Seccion 4

--- assets/js/bases/logica-booleana.js
+++ assets/js/bases/logica-booleana.js
@@ -32,3 +32,20 @@ console.log(true || false);
 console.log(false || false);
 console.log(regrsaTrue() || regresaFalse());
 console.log('4 condiciones: ', true || true || true || false);
+
+console.warn("Asignaciones");
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalso = false;
+const a1 = false && 'Hola Mundo' && 150;
+const a2 = 'Hola ' && 'Mundo' && soyFalso;
+const a3 = soyFalso || 'Ya no soy falso';
+const a4 = soyFalso || soyUndefined || soyNull || 'Ya no soy falso de nuevo' || true;
+const a5 = soyFalso || soyUndefined || regrsaTrue() || 'Ya no soy falso de nuevo' || true;
+console.log({a1, a2, a3, a4, a5, });
+
+if(regresaFalse() && regrsaTrue() && true || false || true){
+    console.log('Hace algo');
+} else {
+    console.log('Hace otra cosa');
+}
