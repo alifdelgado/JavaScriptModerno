2017770f4afd2a9076b6bdf933dc9fb333d79b7a
alifdelgado <alifdelgado@gmail.com>
2020-04-06 06:52:23 -0500

    Se finaliza el curso

--- 05-promesas-callbacks/src/index.js
+++ 05-promesas-callbacks/src/index.js
@@ -1,12 +1,19 @@
-import './styles.css';
-import { buscarHeroe } from './js/callbacks';
-const heroeId = 'capi';
+import { obtenerHeroesArr, obtenerHeroeAwait, heroesCiclo, heroesIfAwait } from './js/await';
 
-buscarHeroe(heroeId, (err, heroe) => {
-    if(err) {
-        console.error(err);
-    } else {
-        console.log(heroe);
-        
-    }
-});
+heroesCiclo();
+heroesIfAwait('iron');
+// obtenerHeroesArr().then(console.table);
+
+// obtenerHeroeAwait('capi2').then(console.log).catch(console.warn);
+
+// import { buscarHeroe, buscarHeroeAsync } from './js/promesas';
+// import { promesaLenta, promesaMedia, promesaRapida } from './js/promesas';
+
+// buscarHeroe('capi').then(console.log).catch(console.warn);
+// buscarHeroeAsync('iron2').then(console.log).catch(console.warn);
+
+// promesaLenta.then(console.log);
+// promesaMedia.then(console.log);
+// promesaRapida.then(console.log);
+
+// Promise.race([promesaLenta, promesaMedia, promesaRapida]).then(console.log);
